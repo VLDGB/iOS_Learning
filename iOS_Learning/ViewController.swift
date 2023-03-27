8c3aee582b401d84105c379c2a9bc410e4d2f4b6
Vlad Rosca <vlad.rosca@nagarro.com>
2023-03-27 13:54:51 +0300

    Initial Commit

---
+++ iOS_Learning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Learning
+//
+//  Created by Vlad_Rosca on 27.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
